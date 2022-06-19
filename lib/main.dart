e23a2ac6417c0b27434749ad4807a22a0210f236
heeeun <gml7868@silverstar-solution.com>
2022-06-20 01:17:52 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,155 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      MaterialApp(
+          home:MyApp()
+      )
+  );    // app 시작
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAppState extends State<MyApp> {
+  int a = 1;
+  var name = ["김영숙", "정은희", "김희은"];
+  var phone = ["000-000-0000", "000-5388-7868", "010-4388-7868"];
+  var total = 3;
 
-  void _incrementCounter() {
+  addOne(value) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      total++;
+      name.add(value);
+      name.sort();
+    });
+  }
+
+  deleteOne(value) {
+    setState(() {
+      name.removeAt(value);
+      name.sort();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+        appBar: AppBar(title: Text(total.toString())),
+        body: ShopItem(name: name, phone: phone, deleteOne: deleteOne),
+        bottomNavigationBar: BottomCustomWidget(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print(context.findAncestorWidgetOfExactType<MaterialApp>());
+            showDialog(context: context, builder: (context) {
+                  return CustomDialog(addOne: addOne);
+                }
+            );
+          },
+        ),
+      );
+  }
+}
+
+class CustomDialog extends StatelessWidget {
+  CustomDialog({Key? key, this.addOne}) : super(key: key);
+
+  final addOne;
+  var inputDate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SizedBox(
+        width: 300,
+        height: 300,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            TextField(
+              onChanged: (input) {
+                inputDate = input;
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            TextButton(
+                onPressed: () {
+                  if(inputDate != null || inputDate.isNotEmpty) {
+                    addOne(inputDate);
+                    Navigator.pop(context);
+                  }
+                },
+                child: Text('완료')
             ),
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('취소')
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
+
+
+class BottomCustomWidget extends StatelessWidget {
+  const BottomCustomWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: SizedBox(
+        height: 70,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.phone),
+            Icon(Icons.message),
+            Icon(Icons.account_balance_wallet_rounded)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ShopItem extends StatefulWidget {
+  ShopItem({Key? key, this.name, this.phone, this.deleteOne}) : super(key: key);
+  var name;
+  var phone;
+  var deleteOne;
+
+  @override
+  State<ShopItem> createState() => _ShopItemState();
+}
+
+class _ShopItemState extends State<ShopItem> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: widget.name.length,
+        itemBuilder: (context, i) {
+          return Card(
+            child: ListTile(
+              leading: Image.asset("aaa.png"),
+              title: Text(widget.name[i] + "   " + widget.phone[i]),
+              trailing: TextButton(
+                  onPressed: () {
+                    widget.deleteOne(i);
+                  },
+                  child: Text('삭제')
+              ),
+            ),
+          );
+        }
+    );
+  }
+}
+
